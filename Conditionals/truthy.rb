691151507d222dc3d2b3ac274b5a5f439591886f
Rami Aidy <ramiaidy@icloud.com>
2018-07-21 15:53:37 -0400

    completed Conditionals of Ruby Basics

---
+++ Conditionals/truthy.rb
@@ -0,0 +1,8 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+# in Ruby, every expression evaluates to true when used in a condition, exception: false and nil.
